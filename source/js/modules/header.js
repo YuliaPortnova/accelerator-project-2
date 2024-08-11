e92950a60043cc61d3a2674d5b33631e01e2fd4c
YuliaPortnova <yuyul@yandex.ru>
2024-08-11 23:26:17 +0300

    feat: добавляет открытие меню при переходах табом

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,18 +1,49 @@
 const openButton = document.querySelector('.page-header__burger-button');
 const header = document.querySelector('.page-header');
 const links = header.querySelectorAll('.page-header__link');
+const menu = header.querySelector('.page-header__nav');
+
+const closeMenu = () => {
+  document.body.setAttribute('style', 'overflow: hidden');
+  header.classList.remove('is-open');
+}
+
+const openMenu = () => {
+  header.classList.add('is-open');
+  document.body.removeAttribute('style');
+}
 
 openButton.addEventListener('click', () => {
-  header.classList.toggle('is-open');
-
   if (header.classList.contains('is-open')) {
-    document.body.setAttribute('style', 'overflow: hidden');
-  } else document.body.removeAttribute('style');
+    closeMenu();
+  } else {
+    openMenu();
+  }
 });
+
+const onMenuFocusIn = () => {
+  if (document.body.clientWidth <= 768) {
+    openMenu();
+  }
+}
+
+const onMenuFocusOut = () => {
+  if (document.body.clientWidth <= 768) {
+    closeMenu();
+  }
+  menu.removeEventListener("focusin", onMenuFocusIn);
+  menu.removeEventListener("focusout", onMenuFocusOut);
+}
 
 links.forEach((link) => {
   link.addEventListener('click', () => {
     header.classList.remove('is-open');
     document.body.removeAttribute('style');
   })
+  link.addEventListener('focus', () => {
+    menu.addEventListener("focusin", onMenuFocusIn);
+    menu.addEventListener("focusout", onMenuFocusOut);
+  })
 });
+
+
