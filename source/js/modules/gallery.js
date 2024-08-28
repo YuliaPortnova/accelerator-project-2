187e262d8c0227611213bbe4be2a46991f052055
YuliaPortnova <yuyul@yandex.ru>
2024-08-28 06:41:04 +0300

    fix: чинит активацию слайдера для мобильного и планшета

--- source/js/modules/gallery.js
+++ source/js/modules/gallery.js
@@ -3,32 +3,37 @@ import {Navigation} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-new Swiper('.gallery', {
-  modules: [Navigation],
-  watchOverflow: true,
-  spaceBetween: 5,
-  speed: 600,
-  lazy: true,
-  lazyPreloadPrevNext: 1,
-  loop: true,
-  slidesPerView: 'auto',
-  simulateTouch: true,
-  grabCursor: true,
+let gallerySlider;
 
-  navigation: {
-    nextEl: '.gallery__slider-buttons .swiper-button-next',
-    prevEl: '.gallery__slider-buttons .swiper-button-prev',
-  },
+const createGallerySlider = () => {
+  gallerySlider = new Swiper('.gallery', {
+    modules: [Navigation],
+    watchOverflow: true,
+    spaceBetween: 5,
+    speed: 600,
+    lazy: true,
+    lazyPreloadPrevNext: 1,
+    loop: true,
+    slidesPerView: 'auto',
+    simulateTouch: true,
+    grabCursor: true,
 
-  breakpoints: {
-    320: {
-      enabled: true,
+    navigation: {
+      nextEl: '.gallery__slider-buttons .swiper-button-next',
+      prevEl: '.gallery__slider-buttons .swiper-button-prev',
     },
-    768: {
-      enabled: true,
-    },
-    1440: {
-      enabled: false,
-    }
+  });
+}
+
+const breakpoint = window.matchMedia("(max-width: 1439px)");
+
+const onBreakpointChange = () => {
+  if (breakpoint.matches) {
+    createGallerySlider();
+  } else {
+    gallerySlider?.destroy();
   }
-});
+};
+
+breakpoint.addEventListener("change", onBreakpointChange);
+onBreakpointChange();
